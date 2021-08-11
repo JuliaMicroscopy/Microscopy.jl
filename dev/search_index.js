6a46d2570b9c7aa2e39f03d7b200e866e60ee477
Documenter.jl <documenter@juliadocs.github.io>
2021-08-11 08:23:09 +0000

    build based on 2bff0a9

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = Microscopy","category":"page"},{"location":"#Microscopy","page":"Home","title":"Microscopy","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for Microscopy.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [Microscopy]","category":"page"}]
+}
